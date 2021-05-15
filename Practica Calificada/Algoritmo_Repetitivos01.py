5311ce5e0ec3faa3f6112011cdd122edd69eb4dd
Isaac-saavedra <75545837+Isaac-saavedra@users.noreply.github.com>
2021-05-14 19:41:06 -0500

    Delete Practica Calificada directory

--- Practica Calificada/Algoritmo_Repetitivos01.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Ingrese numero del 1 -12: "))
-cont = int
-
-for cont in range(1,12):
-    print (cont, " X ", num, " = ", (cont * num))
--- Practica Calificada/Algoritmo_Secuencial02.py
+++
@@ -1,4 +0,0 @@
-soles = int(input("Ingrese el monto en soles "))
-#print(soles)
-print ("DOLAR: ", soles / 2.7)
-print ("EUROS: ", soles / 4)
